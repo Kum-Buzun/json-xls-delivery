1ffc4999e477ee0d2eacb08d377e51fce182b693
Thirunavukkarasu <thirunar@thoughtworks.com>
2013-11-27 15:47:16 +0530

    Thiru & Kiran  | #756 | Changed deploy scripts to deploy shadow jar.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 [[ -z "$1" ]] && echo "Usage: $0 version-of-json-to-xls-delivery-to-deploy" >&2 && exit 1
 
 VERSION=$1
-FILENAME="json-to-xls-${VERSION}-standalone.jar"
+FILENAME="json-to-xls-${VERSION}-shadow.jar"
 
 #copying json-to-xls.yml to Prod
 scp json-to-xls.yml prod://home/motech/config-json-to-xls.yml
@@ -14,5 +14,5 @@ ssh prod '\
     export time=`date +%F_%T`; \
     mkdir -p old/backup-json-to-xls.${time} && \
     ([[ `ls | grep -q "json-to-xls.*"; echo $?` = 0 ]] && /bin/mv -f json-to-xls* old/backup-json-to-xls.${time} || true) && \
-    wget http://nexus.motechproject.org/content/repositories/json-to-xls/io/ei/jsontoxls/json-to-xls/0.2-SNAPSHOT/'${FILENAME}' && \
+    wget http://nexus.motechproject.org/content/repositories/json-to-xls/io/ei/jsontoxls/json-to-xls/'${VERSION}'/'${FILENAME}' && \
     java -Ddw.http.port=9090 -jar '${FILENAME}' server config-json-to-xls.yml'
